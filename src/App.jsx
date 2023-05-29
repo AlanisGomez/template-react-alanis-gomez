7c026c958ca78f638443cc6d0f7ac254de197f3a
Alanis Gomez <agomez@aper.com>
2023-05-29 20:49:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import Card from "./Components/Card";
+import './App.css'
+
+function App() {
+  // const [name, setName] = useState('')
+  return (
+    <>
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+    </>
+  )
+}
+
+export default App
