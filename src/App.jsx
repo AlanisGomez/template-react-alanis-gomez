b274b0f3b596c362b096d1034dbc30a416b71661
Alanis Gomez <agomez@aper.com>
2023-05-31 19:51:26 -0300

    Agregando formulario para parcial de front

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import Card from "./Components/Card";
 import './App.css'
 
 function App() {
-  // const [name, setName] = useState('')
+  const [name, setName] = useState('')
+  const [character, setCharacter] = useState('')
+  const [error, setError] = useState(false);
+  const [showCard, setShowCard] = useState(false);
+
+  const handleForm = (e) => {
+    e.preventDefault();
+  
+    if (name.length < 3 || name.startsWith(' ') || character.length < 6) {
+      setError(true);
+      setShowCard(false);
+    } else {
+      setError(false);
+      setShowCard(true);
+    }
+  };
+  
   return (
     <>
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <h1>Personaje favorito de The Office</h1>
+      <div className="formContainer">
+        <form onSubmit={handleForm}>
+          <input type="text" placeholder="Ingresá tu nombre" onBlur={(e) => setName(e.target.value)}/>
+          <input type="text" placeholder="Ingresá tu personaje favorito de la serie" onBlur={(e) => setCharacter(e.target.value)}/>
+          <button type="sumit">Enviar</button>
+        </form>
+      </div>
+      {error && <p className='alert'>Por favor chequea que la información sea correcta</p>}
+      {showCard && <Card name={name} character={character}/>}
     </div>
     </>
   )
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+const Card = ({name, character}) => {
+  return (
+      <>
+      <div className="card">
+          <div>Hola <b>{name}!</b></div>
+          <div>Sabemos que tu personaje favorito de la serie es <b>{character}</b></div>
+      </div>
+      </>
+  )
 };
 
 
